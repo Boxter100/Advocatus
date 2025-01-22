d4b410e33002f7dc5ef8e2f28b5dee98750bfc6c
Boxter100 <jared.nicolas779@gmail.com>
2025-01-21 19:47:36 -0600

    areas barras

--- JS/flechas.js
+++ JS/flechas.js
@@ -9,7 +9,7 @@ document.getElementById('asesoriaLegalToggle').addEventListener('click', () => {
   arrow.classList.toggle('rotate-180'); // Rota la flecha
   
   // Cambiar transición suave
-  content.style.transition = 'max-height 0.5s ease';
+  content.style.transition = 'max-height 0.4s ease';
 });
 
 // Toggle para Representación legal
@@ -23,5 +23,49 @@ document.getElementById('representacionLegalToggle').addEventListener('click', (
   arrow.classList.toggle('rotate-180'); // Rota la flecha
   
   // Cambiar transición suave
-  content.style.transition = 'max-height 0.5s ease';
+  content.style.transition = 'max-height 0.4s ease';
 });
+
+
+
+// Toggle para Áreas
+document.getElementById('areasToggle').addEventListener('click', () => {
+  const content = document.getElementById('areasContent');
+  const arrow = document.getElementById('areasArrow');
+
+  // Alternar visibilidad y dirección de la flecha
+  content.classList.toggle('max-h-0');
+  content.classList.toggle('max-h-screen'); // Ajusta la altura máxima que desees
+  arrow.classList.toggle('rotate-180'); // Gira la flecha
+
+  // Aplicar transición suave
+  content.style.transition = 'max-height 0.4s ease-in-out'; // Cambiar según la duración y suavidad deseada
+
+  // Iniciar la animación de las barras de progreso
+  if (!content.classList.contains('max-h-0')) {
+    startProgressAnimation();
+  }
+});
+
+// Función para iniciar la animación de las barras de progreso
+const startProgressAnimation = () => {
+  // Definir los valores de los anchos de las barras (en porcentaje)
+  const targetWidths = [70, 70, 80, 90, 100, 100, 100, 70]; // Cambiar según los valores de tus barras
+
+  // Seleccionar solo las barras de progreso que tienen la clase 'progress-bar'
+  const progressBars = document.querySelectorAll('.progress-bar');
+  progressBars.forEach((progressBar, index) => {
+    // Restablecer el ancho de la barra antes de la animación
+    progressBar.style.width = '0%';
+
+    // Iniciar la animación de llenado
+    setTimeout(() => {
+      progressBar.style.transition = 'width 2s ease'; // Establecer la duración de la animación
+      progressBar.style.width = `${targetWidths[index]}%`; // Ajustar el ancho según el valor
+    }, 100); // Tiempo de espera antes de comenzar la animación
+  });
+};
+
+
+
+
